031195e14a717b9425c93d98639940e1701e4808
Kevin Chi <kevinchi118@gmail.com>
2018-06-20 20:59:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,18 @@
 class Artist
-    attr_accessor :name 
+    attr_accessor :name :songs
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def add_song(song)
+        song.artist = self
+        self.songs << song
+    end
+
+    def self.song_count
+        @songs
+    end 
+
 end
