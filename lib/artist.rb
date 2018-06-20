2f490a1604d630b297d8c339f9cc41ed2b0787d0
Kevin Chi <kevinchi118@gmail.com>
2018-06-20 21:15:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ class Artist
         song.artist = self
     end
 
-    def add_song_by_name(song)
-        song = Song.new(song)
+    def add_song_by_name(name)
+        song = Song.new(name)
         self.songs << song
         song.artist = self
     end
